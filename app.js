9a3faac6e7f6393dbbda82cf3cc8fe8b96d4f035
bhavya shree <bhavyashree@enkefalos.com>
2022-03-21 14:58:04 +0530

    changes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app =express()
 
 app.get("/", function(req,res) {
     res.send("It Worked")
+});
+app.get("/new",function(req, res){
+    res.semd("New also worked");
 })
 
 app.listen(process.env.PORT || 5000)
